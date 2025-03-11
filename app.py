3f24b21019385bcd2bf65e67e8b344e93eee06ae
WhileTrueFrank <whillercf@gmail.com>
2025-03-11 13:08:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,6 @@ def leaderboard():
     
     return render_template('leaderboard.html', team_scores=team_scores)
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
